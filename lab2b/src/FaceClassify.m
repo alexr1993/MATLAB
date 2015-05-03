be354fac330f1c6e0021f1b69559f23c746260a2
Alex Remedios <alexr1993@gmail.com>
2015-05-03 18:58:09 +0100

    Attempt grid search improvement for SVM

--- lab2b/src/FaceClassify.m
+++ lab2b/src/FaceClassify.m
@@ -2,10 +2,9 @@
 
 % Configuration
 grayscale = 0;
-nearestNeighbour = 1;
+nearestNeighbour = 0;
 supportVectorMachine = 1;
-svm_params = '-s 1 -t 1 -d 3';
-svm_params = '';
+svm_params = '-q -s 1 -t 1 -d 3 -c 8 -g 1';
 
 % Globals
 names={'barroso','calderon','cameron','erdogan','gillard','harper', ...
@@ -100,6 +99,20 @@ if supportVectorMachine == 1
         end;
     end;
 
+%{
+    bestcv = 0;
+    for log2c = -1:3,
+      for log2g = -4:1,
+        cmd = ['-q -q -s 1 -t 1 -d 3 -v 5 -c ', num2str(2^log2c), ' -g ', num2str(2^log2g)];
+        cv = svmtrain(trainingclasses, training_vecs, cmd);
+        if (cv >= bestcv),
+          bestcv = cv; bestc = 2^log2c; bestg = 2^log2g;
+        end
+        fprintf('%g %g %g (best c=%g, g=%g, rate=%g)\n', log2c, log2g, cv, bestc, bestg, bestcv);
+      end
+    end;
+    %}
+ 
     SVMStruct = svmtrain(trainingclasses, training_vecs, svm_params);
 
     % Check accuracy on validation set
